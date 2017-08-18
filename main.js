05a81d3ee44c8919a351284b0ac60bf8a5410df0
v_nosenko <eclipsik69@gmail.com>
2017-08-18 14:00:30 +0300

    added autoSlide

--- main.js
+++ main.js
@@ -1,20 +1,38 @@
-const slider = document.querySelector('.slider');
-const content = slider.querySelector('.content');
-const controls = slider.querySelector('.controls');
-
-	function nextSlide() {
-		controls.children[0].addEventListener('click', function(event) {
-			content.style.transform = 'translateX(-700px)';
-			content.style.transition = '1s';
-		});
-	}
-
-	function prevSlide() {
-		controls.children[1].addEventListener('click', function(event) {
-			content.style.cssText = 'transform:translateX(0)'; 
-			content.style.transition = '1s';
-		});
-	}
-
-console.log(nextSlide());
-console.log(prevSlide());
+(function() {
+	const slider = document.querySelector('.slider');
+	const content = slider.querySelector('.content');
+	const controls = slider.querySelector('.controls');
+	
+	let changeSlideTime = undefined;
+	
+		function nextSlide() {
+			controls.children[0].addEventListener('click', function(event) {
+				content.style.transform = 'translateX(-700px)';
+				changeSlideTime= undefined;
+				});
+			};
+	
+		function prevSlide() {
+			controls.children[1].addEventListener('click', function(event) {
+				content.style.cssText = 'transform:translateX(0)'; 
+				changeSlideTime= undefined;
+				});
+			};
+	
+		function autoSlide() {
+			changeSlideTime = setInterval( function() {
+				content.style.cssText = 'transform:translateX(-700px)';
+			}, 3000);
+	
+			changeSlideTime = setInterval( function() {
+				content.style.cssText = 'transform:translateX(0px)';
+			}, 6000);
+				
+		};
+			
+	
+	console.log(nextSlide());
+	console.log(prevSlide());
+	console.log(autoSlide());
+	
+})();
